780488f3bfe8fc5e2711515cd96d65d753f71d22
Allan Jardine <allan@datatables.net>
2019-10-18 11:06:34 +0000

    Editor 1.9.2

--- DataTables-Editor-Server/Editor.cs
+++ DataTables-Editor-Server/Editor.cs
@@ -110,7 +110,7 @@ namespace DataTables
         /// <summary>
         /// Version string
         /// </summary>
-        public const string Version = "1.9.1";
+        public const string Version = "1.9.2";
 
         /// <summary>
         /// Create a new Editor instance
