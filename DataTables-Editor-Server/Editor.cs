72e6f9a8132ee1ef16e1cd18be98932414b9ce70
Allan Jardine <allan@datatables.net>
2024-03-08 11:41:56 +0000

    Release 2.3.2

--- DataTables-Editor-Server/Editor.cs
+++ DataTables-Editor-Server/Editor.cs
@@ -111,7 +111,7 @@ namespace DataTables
         /// <summary>
         /// Version string
         /// </summary>
-        public const string Version = "2.3.1";
+        public const string Version = "2.3.2";
 
         /// <summary>
         /// Create a new Editor instance
